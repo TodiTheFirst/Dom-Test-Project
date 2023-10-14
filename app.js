6c634a20d33a5296bb1993170fb6b76df834e4f8
TodiTheFirst <146870436+TodiTheFirst@users.noreply.github.com>
2023-10-14 17:41:12 +0300

    Update app.js

--- app.js
+++ app.js
@@ -37,3 +37,27 @@ button.addEventListener('click', () => {
 section.classList.toggle('visible');
 section.classList.toggle('invisible');
 });
+// function solve() {
+//     document.querySelector('ul').innerHTML = '<li>Learn JavaScript</li><li>Master JavaScript</li>';
+// }                     
+        // создание элементов для Html первый способ
+// const secondGoal = document.createElement('li');
+// secondGoal.textContent = 'Master JavaScript';
+// document.querySelector('ul').appendChild(secondGoal);
+       // создание элементов для html  второй способ
+const list = document.querySelector('ul');
+const newLi = document.createElement('li');
+newLi.textContent = 'Item 4';
+list.prepend(newLi); 
+ //в отличии от .appendeChilde добавляет новый элемент не в начале а в конце
+ list.lastElementChild.before(newLi) 
+  //befor также как after перемещает элемент html соотвественно туда куда написано
+list.firstElementChild.replaceWith(newLi)
+//replaceWith заменяет элемент вместо того чтобы его просто перемещать
+const newLi2 = newLi.cloneNode(true)
+// клонирует элемент  если true то копирует все childe элементы если false то только сам элемент
+list.append(newLi, newLi2)
+// const listse =  document.querySelector('ul');
+// listse.remove();
+ // позволяет убрать элемент из запаршенной страницы в html
+
